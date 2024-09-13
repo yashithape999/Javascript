b42122b3958a28622f844c940db8b632b7eedae5
yashithape999 <yashithape999@gmail.com>
2024-09-13 12:18:13 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs not browser syntax for alert is different in nodejs.
+ console.log(3
+    +3)//code redability should be high
+
+ console.log("Yash")  // tc39(ECMA) and MDN for documentation reading 
+ 
