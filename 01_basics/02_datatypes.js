fdafa7aba329bdebc463f3d939cbc526c6e998fd
yashithape999 <yashithape999@gmail.com>
2024-09-24 14:22:06 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,4 +5,24 @@
     +3)//code redability should be high
 
  console.log("Yash")  // tc39(ECMA) and MDN for documentation reading 
+
+ let name="Yash"
+ let age=18
+ let isLoggedIn=false
+
+
+ //PRIMITIVE DATA TYPES
+//number > 2 to the power 53 (Range of the number)
+//bigint > (used to store big numbers ex.trading platforms) 
+//string > " "(we can also write in single quotes)
+//boolean > True/False
+//null > standalone value
+//undefined > Value is not assigned yet 
+//Symbol > Represent uniqueness
+
+ //object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //Object
+
  
