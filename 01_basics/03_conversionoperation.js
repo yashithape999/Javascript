b5b79fcd423052a9939406f344e4851ec66b560e
yashithape999 <yashithape999@gmail.com>
2024-09-25 06:19:44 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score="Yash"
+
+//console.log(typeof(score));
+//console.log(typeof score);
+
+let valueInnumber=Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//"43" > 43
+//"43abc" > NaN
+// true>1 false>0
+
+let isLoggedIn=1
+let booleanisLoggedin=Boolean(isLoggedIn)
+//console.log(booleanisLoggedin);
+
+//1>true
+//0>false
+//"">false
+//"Yash">true
+
+let someNumber= 43
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
